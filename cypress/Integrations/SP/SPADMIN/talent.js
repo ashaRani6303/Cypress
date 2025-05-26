3ca2a60d72fb47190109255c85b4255667ca8c48
ashaRani6303 <ranivajrapu6303@gmail.com>
2025-05-26 18:36:10 +0530

    add talent changes

--- cypress/Integrations/SP/SPADMIN/talent.js
+++ cypress/Integrations/SP/SPADMIN/talent.js
@@ -10,9 +10,9 @@ describe('Add talent case', function () {
       cy.wait(8000);
       cy.get(".ah-quick-search-main-title").contains("Talent Hub").click();
       //when there is text by using class name we can write like this
-      cy.get(".ah-btn.ah-btn-icon").contains("Add").click();
+      /*cy.get(".ah-btn.ah-btn-icon").contains("Add").click();
       cy.wait(4000);
-      cy.get("[formcontrolname='name']").type('Cypress talent 5').should('have.value', 'Cypress talent 5').click(); //this is attribute selector
+      cy.get("[formcontrolname='name']").type('Cypress test talent').should('have.value', 'Cypress test talent').click(); //this is attribute selector
       cy.get(".ah-unselect-text").click();
       cy.wait(4000);
       cy.get('.ah-emp-search > .input-group > .form-control').type("TEST008{enter}").should('have.value', 'TEST008');
@@ -27,9 +27,12 @@ describe('Add talent case', function () {
           cy.log("Modal is closed and done");
         }
       })
-      cy.get("#request-cancel").contains('Confirm').click();
+      cy.get("#request-cancel").contains('Confirm').click(); 
+    
       cy.contains('Talent has been added successfully!').should('be.visible');
-      cy.get("#request-submit-close").click();
+      cy.get("#request-submit-close").click(); */
+      cy.get("#column-search-input").type('Cypress test talent{enter}').click();
+      cy.get("#checkinduration").click();
 
     })
   })
