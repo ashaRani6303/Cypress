6077e3c7a0490ceea044dcd89bba1c608ebc968d
ashaRani6303 <ranivajrapu6303@gmail.com>
2025-06-09 17:52:39 +0530

    admin changes

--- cypress/Integrations/SP/SPADMIN/talent.js
+++ cypress/Integrations/SP/SPADMIN/talent.js
@@ -2,18 +2,11 @@ import Talenthub from '../../../support/pageObjects/Admin/SP/talenthub';
 import Admin from '../../../support/pageObjects/Admin/admin';
 describe('Add talent case', function () {
   BeforeEach(function () {
-
+    Admin.adminLogin();
+    cy.wait(5000);
+    Admin.selectModule("Succession", "Talent Hub");
   })
   it('Add talent Success Case', function () {
-    let data = "";
-    cy.fixture('example').then((fdata) => {
-      data = fdata;
-      adminLogin(data);
-      selectModule("Succession", "Talent Hub");
-      // addTalent("Cypress test talent", "test008");
-      searchAndVerifyTalent("Cypress test talent");
-      verifyTalentDetails({ talentName: "Cypress test talent", talentOwner: "Rohit" });
-      updateTalent();
-    })
+    Talenthub.addTalent
   })
 })
